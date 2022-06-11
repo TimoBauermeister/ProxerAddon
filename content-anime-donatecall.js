3072c86fdc66785a29cb4fe8540357d0a50ea9c4
Luca Kleck <39993797+LucaKleck@users.noreply.github.com>
2022-06-12 01:14:46 +0200

    auto donate ask skipper

---
+++ content-anime-donatecall.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    if(document.getElementById("donatecall") != undefined) {
+        for(let aElem of document.getElementById("donatecall").getElementsByTagName("A")) {
+            if(aElem.textContent==="Später erinnern") {
+                aElem.click();
+            }
+        }
+    }
+}, 200);
--- content-anime-theater.js
+++ content-anime-theater.js
@@ -1,3 +1,11 @@
+// trigger donatecall remove event
+let s = document.createElement('script');
+s.src = chrome.runtime.getURL('content-anime-donatecall.js');
+s.onload = function() {
+    
+};
+(document.head || document.documentElement).appendChild(s);
+
 chrome.storage.sync.get(['theaterMode'], function (result) {
     if(result && result['theaterMode']) {
         let link = document.createElement("link");
@@ -8,62 +16,63 @@ chrome.storage.sync.get(['theaterMode'], function (result) {
         setTimeout(() => {
             let divNewButtonsTop = document.createElement("div");
             divNewButtonsTop.setAttribute("id", "topButtonMenu");
-
-            let navBtns = document.getElementsByClassName("no_details")[0].getElementsByTagName("A");
-            let naechste = document.getElementsByClassName("no_details")[0].getElementsByClassName("menu");
-            let streamBtns = document.getElementsByClassName("wMirror")[0].getElementsByClassName("menu");
-            let detailAndEpisodes = document.getElementById("simple-navi").getElementsByTagName("A");
-            let nextHref = "";
-            for(let elem of naechste) {
-                if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
-                    nextHref = elem.href;
-                }
-            }
-            for(let elem of navBtns) {
-                if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
-                    nextHref = elem.href;
-                }
-                if(elem.textContent.includes("Watchlist")) {
-                    if(elem.textContent.includes("Nächste")) {
-                        elem.textContent = "Nächste und abhaken >";
-                        elem.addEventListener("click", function () {
-                            let lastPageLink = 'https://proxer.me' + getHREF();
-                            chrome.storage.sync.set({ 'lastPageLinkAnime': lastPageLink }, function () {
-                            });
-                        
-                            let lastPageTitle = document.title;
-                            lastPageTitle = lastPageTitle.replace(" - Proxer.Me", "");
-                            let lastPageTitle_array = lastPageTitle.split("Episode ");
-                            let number = lastPageTitle_array[1].substring(0, lastPageTitle_array[1].indexOf(" "));
-                            lastPageTitle = lastPageTitle_array[0] + 'Episode ' + (parseInt(number) + 1) + lastPageTitle_array[1].substring(number.length);
-                        
-                            chrome.storage.sync.set({ 'lastPageTitleAnime': lastPageTitle }, function () {
-                            });
-                            if(nextHref !== "") {
-                                location.href = nextHref;
-                            }
-                        });
-                        divNewButtonsTop.append(elem);
-                    }
-                } else {
-                    if(elem.textContent.includes("Vorherige")) {
-                        divNewButtonsTop.append(elem);
+            if(document.getElementsByClassName("no_details") !== "undefined") {
+                let navBtns = document.getElementsByClassName("no_details")[0].getElementsByTagName("A");
+                let naechste = document.getElementsByClassName("no_details")[0].getElementsByClassName("menu");
+                let streamBtns = document.getElementsByClassName("wMirror")[0].getElementsByClassName("menu");
+                let detailAndEpisodes = document.getElementById("simple-navi").getElementsByTagName("A");
+                let nextHref = "";
+                for(let elem of naechste) {
+                    if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
+                        nextHref = elem.href;
                     }
                 }
-            
+
+                for(let elem of navBtns) {
+                    if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
+                        nextHref = elem.href;
+                    }
+                    if(elem.textContent.includes("Watchlist")) {
+                        if(elem.textContent.includes("Nächste")) {
+                            elem.textContent = "Nächste und abhaken >";
+                            elem.addEventListener("click", function () {
+                                let lastPageLink = 'https://proxer.me' + getHREF();
+                                chrome.storage.sync.set({ 'lastPageLinkAnime': lastPageLink }, function () {
+                                });
+                            
+                                let lastPageTitle = document.title;
+                                lastPageTitle = lastPageTitle.replace(" - Proxer.Me", "");
+                                let lastPageTitle_array = lastPageTitle.split("Episode ");
+                                let number = lastPageTitle_array[1].substring(0, lastPageTitle_array[1].indexOf(" "));
+                                lastPageTitle = lastPageTitle_array[0] + 'Episode ' + (parseInt(number) + 1) + lastPageTitle_array[1].substring(number.length);
+                            
+                                chrome.storage.sync.set({ 'lastPageTitleAnime': lastPageTitle }, function () {
+                                });
+                                if(nextHref !== "") {
+                                    location.href = nextHref;
+                                }
+                            });
+                            divNewButtonsTop.append(elem);
+                        }
+                    } else {
+                        if(elem.textContent.includes("Vorherige")) {
+                            divNewButtonsTop.append(elem);
+                        }
+                    }
+                
+                }
+                
+                for(let elem of streamBtns) {
+                    divNewButtonsTop.prepend(elem);
+                }
+
+                for(let elem of detailAndEpisodes) {
+                    divNewButtonsTop.prepend(elem);
+                }
+
+                divNewButtonsTop.prepend(document.getElementsByClassName("wName")[0]);
+                document.body.appendChild(divNewButtonsTop);
             }
-
-            for(let elem of streamBtns) {
-                divNewButtonsTop.prepend(elem);
-            }
-
-            for(let elem of detailAndEpisodes) {
-                divNewButtonsTop.prepend(elem);
-            }
-
-            divNewButtonsTop.prepend(document.getElementsByClassName("wName")[0]);
-            document.body.appendChild(divNewButtonsTop);
-
             setTimeout(() => {
                 document.getElementsByTagName("IFRAME")[0].addEventListener("mousemove", transitionIn(divNewButtonsTop));
             }, 3000);
