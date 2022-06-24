04eb93ea68c28bde7ebe6bf7ba253b8574c096cb
Luca Kleck <39993797+LucaKleck@users.noreply.github.com>
2022-06-24 10:14:18 +0200

    v1.1.0 - inital custom colors, needs regex check

--- loadStylesheet.js
+++ loadStylesheet.js
@@ -1,6 +1,6 @@
 let load = document.createElement("div");
 load.id = "loadPre"
-load.style = "height:100vh; width:100vw; background-color:#000; position: fixed; top:0; left:0; z-index:99;";
+load.style = "height:calc(100vh - 40px); width:100vw; background-color:#000; position: fixed; top:40px; left:0; z-index:99;";
 document.documentElement.appendChild(load);
 searchAndReplace();
 
@@ -14,12 +14,26 @@ function searchAndReplace() {
                 } else if (link.getAttribute("href").includes("black")) {
                     // load grey override for changes in form and color
                     link.setAttribute("href", chrome.runtime.getURL("grey-override-stylesheet.css"));
-                    // create link element and override grey color scheme by changing :root
-                    let rootChanger = document.createElement("link");
-                    rootChanger.href = chrome.runtime.getURL('black-override-stylesheet.css');
-                    rootChanger.type = "text/css";
-                    rootChanger.rel = "stylesheet";
-                    link.parentElement.appendChild(rootChanger);
+                    // set root to user colors
+                    chrome.storage.sync.get({
+                        customColors:["#000","#045b62","#0f565e","#02393e","#0d484e","#3d8086"]
+                    },
+                    function(data) {
+                       //console.log(data.customColors);
+                       document.documentElement.setAttribute("style",
+                        `
+                        --body-background: ` + data.customColors[0] + `;
+                        --element: `         + data.customColors[1] + `;
+                        --element-accent: `  + data.customColors[2] + `;
+                        --element-darkend: ` + data.customColors[3] + `;
+                        --main: `            + data.customColors[4] + `;
+                        --main-accent: `     + data.customColors[5] + `;
+                        `
+                   )
+                       update(data.customColors); //storing the storage value in a variable and passing to update function
+                    }
+                    );
+                    
                     return;
                 } else if (link.getAttribute("href").includes("blue")) {
                     // load grey override for changes in form and color
@@ -40,4 +54,11 @@ function searchAndReplace() {
         searchAndReplace();
     }, 10);
     
+}
+function update(array) {
+    chrome.storage.sync.set({
+        customColors:array
+    }, function() {
+        console.log("added to list with new values");
+    });
 }
--- popup.js
+++ popup.js
@@ -1,3 +1,50 @@
+/*Custom Color*/
+chrome.storage.sync.get({
+    customColors:["#000","#045b62","#0f565e","#02393e","#0d484e","#3d8086"]
+},
+function(data) {
+    //console.log(data.customColors);
+    /* input read write here */
+    let body_background = document.getElementById("body_background");
+    body_background.value = data.customColors[0];
+    body_background.onchange = function() {
+        data.customColors[0] = body_background.value;
+        update(data.customColors);
+    }
+    let element = document.getElementById("element");
+    element.value = data.customColors[1];
+    element.onchange = function() {
+        data.customColors[1] = element.value;
+        update(data.customColors);
+    }
+    let element_accent = document.getElementById("element_accent");
+    element_accent.value = data.customColors[2];
+    element_accent.onchange = function() {
+        data.customColors[2] = element_accent.value;
+        update(data.customColors);
+    }
+    let element_darkend = document.getElementById("element_darkend");
+    element_darkend.value = data.customColors[3];
+    element_darkend.onchange = function() {
+        data.customColors[3] = element_darkend.value;
+        update(data.customColors);
+    }
+    let main = document.getElementById("main");
+    main.value = data.customColors[4];
+    main.onchange = function() {
+        data.customColors[4] = main.value;
+        update(data.customColors);
+    }
+    let main_accent = document.getElementById("main_accent");
+    main_accent.value = data.customColors[5];
+    main_accent.onchange = function() {
+        data.customColors[5] = main_accent.value;
+        update(data.customColors);
+    }
+}
+);
+
+/*OTHER*/
 let titleAnime = 'No link has been set yet :/';
 let linkAnime = 'No link has been set yet :/'
 let aAnime = document.getElementById("linkAnime");
@@ -42,5 +89,13 @@ chrome.storage.sync.get(['lastPageTitleManga'], function (result) {
 
 function theaterModeToggle() {
     chrome.storage.sync.set({ 'theaterMode': checkbox.checked }, function () {
+    });
+}
+
+function update(array) {
+    chrome.storage.sync.set({
+        customColors:array
+    }, function() {
+        console.log("custom colors saved");
     });
 }
