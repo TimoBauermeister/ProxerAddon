7bcd616f32b8edd5a7918d859b5c60e1e38268aa
Luca Kleck <39993797+LucaKleck@users.noreply.github.com>
2022-07-01 17:06:40 +0200

    v1.1.4 - javascript remake for theater

--- content-anime-theater.js
+++ content-anime-theater.js
@@ -1,13 +1,12 @@
 // trigger donatecall remove event
 let s = document.createElement('script');
 s.src = chrome.runtime.getURL('content-anime-donatecall.js');
-s.onload = function() {
-    
-};
+s.onload = function() {};
 (document.head || document.documentElement).appendChild(s);
 
 chrome.storage.sync.get(['theaterMode'], function (result) {
     if(result && result['theaterMode']) {
+        var nextHref = "";
         let link = document.createElement("link");
         link.href = chrome.runtime.getURL('content-anime-theater-stylesheet.css');
         link.type = "text/css";
@@ -16,83 +15,64 @@ chrome.storage.sync.get(['theaterMode'], function (result) {
         setTimeout(() => {
             let divNewButtonsTop = document.createElement("div");
             divNewButtonsTop.setAttribute("id", "topButtonMenu");
-            if(document.getElementsByClassName("no_details") !== "undefined") {
-                let navBtns = document.getElementsByClassName("no_details")[0].getElementsByTagName("A");
-                let naechste = document.getElementsByClassName("no_details")[0].getElementsByClassName("menu");
-                let streamBtns = document.getElementsByClassName("wMirror")[0].getElementsByClassName("menu");
-                let detailAndEpisodes = document.getElementById("simple-navi").getElementsByTagName("A");
-                let nextHref = "";
-                for(let elem of naechste) {
-                    if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
-                        nextHref = elem.href;
-                    }
+            let table = document.getElementById("wContainer");
+            while(table.querySelector(".menu") != undefined) {
+                let elem = table.querySelector(".menu");
+                // next chapter
+                if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
+                    elem.setAttribute("class", elem.getAttribute("class") + " next");
+                    nextHref = elem.href;
+                    elem.innerHTML = "&rarr;";
                 }
-
-                for(let elem of navBtns) {
-                    if(elem.textContent.includes("Nächste") && !elem.textContent.includes("Watchlist")) {
-                        nextHref = elem.href;
-                    }
-                    if(elem.textContent.includes("Watchlist")) {
-                        if(elem.textContent.includes("Nächste")) {
-                            elem.innerHTML = "&#10004 + &rarr;";
-                            elem.addEventListener("click", function () {
-                                let lastPageLink = 'https://proxer.me' + getHREF();
-                                chrome.storage.sync.set({ 'lastPageLinkAnime': lastPageLink }, function () {
-                                });
-                            
-                                let lastPageTitle = document.title;
-                                lastPageTitle = lastPageTitle.replace(" - Proxer.Me", "");
-                                let lastPageTitle_array = lastPageTitle.split("Episode ");
-                                let number = lastPageTitle_array[1].substring(0, lastPageTitle_array[1].indexOf(" "));
-                                lastPageTitle = lastPageTitle_array[0] + 'Episode ' + (parseInt(number) + 1) + lastPageTitle_array[1].substring(number.length);
-                            
-                                chrome.storage.sync.set({ 'lastPageTitleAnime': lastPageTitle }, function () {
-                                });
-                                if(nextHref !== "") {
-                                    location.href = nextHref;
-                                }
-                            });
-                            divNewButtonsTop.append(elem);
+                // previous chapter
+                if(elem.textContent.includes("Vorherige")) {
+                    elem.setAttribute("class", elem.getAttribute("class") + " previous");
+                    elem.innerHTML = "&larr;";
+                }
+                // if next chapter + watchlist
+                if(elem.getAttribute("title") == "reminder_next") {
+                    elem.setAttribute("class", elem.getAttribute("class") + " reminderNext");
+                    elem.innerHTML = "&#10004 + &rarr;";
+                    elem.addEventListener("click", function () {
+                        let lastPageLink = 'https://proxer.me' + getHREF();
+                        chrome.storage.sync.set({ 'lastPageLinkAnime': lastPageLink }, function () {
+                        });
+                    
+                        let lastPageTitle = document.title;
+                        lastPageTitle = lastPageTitle.replace(" - Proxer.Me", "");
+                        let lastPageTitle_array = lastPageTitle.split("Episode ");
+                        let number = lastPageTitle_array[1].substring(0, lastPageTitle_array[1].indexOf(" "));
+                        lastPageTitle = lastPageTitle_array[0] + 'Episode ' + (parseInt(number) + 1) + lastPageTitle_array[1].substring(number.length);
+                    
+                        chrome.storage.sync.set({ 'lastPageTitleAnime': lastPageTitle }, function () {
+                        });
+                        if(nextHref !== "") {
+                            location.href = nextHref;
                         }
-                    } else {
-                        if(elem.textContent.includes("Vorherige")) {
-                            elem.innerHTML = "&larr;";
-                            divNewButtonsTop.append(elem);
-                        }
-                    }
-                
+                    });
                 }
-                
-                for(let elem of streamBtns) {
-                    divNewButtonsTop.prepend(elem);
+                // if bookmark
+                if(elem.getAttribute("title") == "reminder_this") {
+                    elem.setAttribute("class", elem.getAttribute("class") + " reminderThis");
+                    elem.innerHTML = "&#128278;";
                 }
-                let elen = 0;
-                for(let elem of detailAndEpisodes) {
-                    //if(!elem.innerHTML.includes("span class=")) {
-                        elem.setAttribute("id", "navi"+elen)
-                        divNewButtonsTop.append(elem);
-                        elen++;
-                    //}
-                }
-
-                let reminder = document.getElementsByClassName("menu")[1];
-                reminder.innerHTML = "&#128278;";
-                divNewButtonsTop.prepend(reminder);
-
-                let naechsteOhne = document.getElementsByClassName("menu")[1];
-                naechsteOhne.innerHTML = "&rarr;";
-                naechsteOhne.setAttribute("class","menu order6");
-                divNewButtonsTop.prepend(naechsteOhne);
-                
-
-                let textMiddle = document.getElementById("wContainer").childNodes[3].childNodes[0];
-                divNewButtonsTop.prepend(textMiddle);
-                
-                document.body.appendChild(divNewButtonsTop);
+                divNewButtonsTop.append(elem);
             }
-            /* setTimeout(() => {
-                document.getElementsByTagName("IFRAME")[0].addEventListener("mousemove", transitionIn(divNewButtonsTop));
-            }, 3000); */
+            // text
+            let textMiddle = document.getElementById("wContainer").childNodes[3].childNodes[0];
+            textMiddle.setAttribute("class", "textMiddle");
+            divNewButtonsTop.append(textMiddle);
+            // nav buttons
+            let nav = document.getElementById("simple-navi");
+            while(nav.querySelector("a") != undefined) {
+                let elem = nav.querySelector("a");
+                if(elem.querySelector(".wEp")) {
+                    elem.remove();
+                } else {
+                    divNewButtonsTop.append(elem);
+                }
+            }
+            document.body.appendChild(divNewButtonsTop);
         }, 5);
     }
 });
